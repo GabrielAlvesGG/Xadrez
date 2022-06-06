35684b39b22d71e4f576fbbcf76424d30cac2f9b
Gabriel Alves <alves.gabriel7@gmail.com>
2022-06-06 17:18:52 -0300

    Começando a implementar um metodo que imprime o tabuleiro

--- Xadrez/Tabuleiro/Tabuleiro.cs
+++ Xadrez/Tabuleiro/Tabuleiro.cs
@@ -16,5 +16,9 @@
             Colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas [linha, coluna];
+        }
     }
 }
