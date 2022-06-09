c3e7da8f7a85edeb3eee73322d9d9a666a698467
Gabriel Alves <alves.gabriel7@gmail.com>
2022-06-09 15:28:38 -0300

    Implemantando restrições de movimento

--- Xadrez/JogoXadrez/PartidaXadrez.cs
+++ Xadrez/JogoXadrez/PartidaXadrez.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Xadrez.tabuleiro;
+using Xadrez.tabuleiro.TabuleiroException;
 
 
 
@@ -10,8 +11,8 @@ namespace Xadrez.JogoXadrez
     class PartidaXadrez
     {
         public Tabuleiro tab { get; private set; }
-        private int Turno;
-        private Cor JogadorAtual;
+        public int Turno { get; private set; }
+        public Cor JogadorAtual { get; private set; }
         public bool Terminada { get; private set; }
         
 
@@ -30,7 +31,45 @@ namespace Xadrez.JogoXadrez
             Peca capiturada = tab.RetirarPeca(destino);
             tab.ColocarPeca(p, destino);
         }
-        
+        public void RealizaJogada(Posicao origem, Posicao destino)
+        {
+            ExecutaMovimento(origem, destino);
+            Turno++;
+            MudaJogador();
+        }
+        private void MudaJogador()
+        {
+            if(JogadorAtual == Cor.Branca)
+            {
+                JogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                JogadorAtual = Cor.Branca;
+            }
+        }
+        public void ValidarPosicaoOrigem(Posicao pos)
+        {
+            if(tab.peca(pos) == null)
+            {
+                throw new DomainException("Não existe peça nessa posição");
+            }
+            if (JogadorAtual != tab.peca(pos).Cor)
+            {
+                throw new DomainException("A peça de origem não é a da roda atual.");
+            }
+            if (!tab.peca(pos).ExisteMovimentosPossiveis())
+            {
+                throw new DomainException("Não existe movimentos possíveis para essa peça");               
+            }
+        }
+        public void ValidarPosicaoDestino(Posicao origem, Posicao destino)
+        {
+            if (!tab.peca(origem).PodeMoverPara(destino))
+            {
+                throw new DomainException("A execução desse movimento de destino é invalida");
+            }
+        }
         private void ColocarPecas()
         {
             tab.ColocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c',1 ).ToPosicao());
@@ -43,6 +82,7 @@ namespace Xadrez.JogoXadrez
             tab.ColocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 8).ToPosicao());
             tab.ColocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('d', 7).ToPosicao());
             tab.ColocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 8).ToPosicao());
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 7).ToPosicao());
             tab.ColocarPeca(new Rei(tab, Cor.Preta), new PosicaoXadrez('d', 8).ToPosicao());
         }
     }
--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -14,23 +14,34 @@ namespace Xadrez
                 PartidaXadrez partida = new PartidaXadrez();
                 while(!partida.Terminada)
                 {
-                    Console.Clear();
-                    Tela.ImprimirTabuleiro(partida.tab);
+                    try
+                    {
+                        Console.Clear();
+                        Tela.ImprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno :" + partida.Turno);
+                        Console.WriteLine("Aguadando jogada : " + partida.JogadorAtual);
+                        Console.WriteLine();
+                        Console.Write("Digite a posição de origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoOrigem(origem);
 
-                    Console.WriteLine();
-                    Console.Write("Digite a posição de origem: ");
-                    Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
-                    
-                    
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).MovimentosPossiveis();
-                    Console.Clear();
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).MovimentosPossiveis();
+                        Console.Clear();
 
-                    Tela.ImprimirTabuleiro(partida.tab, posicoesPossiveis);
-                    Console.WriteLine();
-                    Console.Write("Digite posição destino: ");
-                    Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
+                        Tela.ImprimirTabuleiro(partida.tab, posicoesPossiveis);
+                        Console.WriteLine();
+                        Console.Write("Digite posição destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDestino(origem, destino); 
 
-                    partida.ExecutaMovimento(origem, destino);
+                        partida.RealizaJogada(origem, destino);
+                    }
+                    catch(DomainException e)
+                    {
+                        Console.WriteLine("Erro !" + e.Message);
+                        Console.ReadLine();
+                    }
                 }
                 
             }
